0edfc0e45522d634c49d0fb7135a296735f6355b
Levi Aul <levi@leviaul.com>
2017-02-16 19:32:14 -0800

    Add Hex package administrivia

--- mix.exs
+++ mix.exs
@@ -6,13 +6,33 @@ defmodule PrettyConsole.Mixfile do
   def project do [
     app: :pretty_console,
     version: @version,
+    description: description(),
+    package: package(),
     elixir: "~> 1.4.0",
-    deps: []
+    deps: deps()
+  ] end
+
+  defp description do
+    """
+    A slightly more pleasant Logger console backend.
+    """
+  end
+
+  defp package do [
+    name: :pretty_console,
+    files: ["config", "lib", "mix.exs", "VERSION", "LICENSE"],
+    maintainers: ["Levi Aul"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/tsutsu/pretty_console"}
   ] end
 
   # Configuration for the OTP application
   def application do [
     mod: {PrettyConsole, []},
     extra_applications: [:logger]
+  ] end
+
+  defp deps do [
+    {:ex_doc, ">= 0.0.0", only: :dev}
   ] end
 end
